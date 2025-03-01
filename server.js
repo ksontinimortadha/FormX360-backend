85ec556a215efa692ff7a422902ad791b8daa555
mortadhaksontini <mortadhaksontini22@gmail.com>
2025-03-01 00:30:22 +0000

    Fixed Vercel deployment

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 
 // Middleware
 app.use(cors());
-
 app.use(bodyParser.json());
 
 // Routes
@@ -18,17 +17,18 @@ app.use("/responses", require("./routes/responseRoutes"));
 app.use("/companies", require("./routes/companyRoutes"));
 
 // Database Connection
-const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+// Server should run independently of MongoDB
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
