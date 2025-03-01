432f2e00323ca008d652d93a7137cda8232f149c
mortadhaksontini <mortadhaksontini22@gmail.com>
2025-03-01 00:21:33 +0000

    updating backend folder1

--- server.js
+++ server.js
@@ -29,3 +29,6 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error(err));
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
